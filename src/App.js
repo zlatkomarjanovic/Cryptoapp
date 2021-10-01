41c200926ed0bd20a4f5ce8118fe92c46e76b422
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-10-01 17:22:32 +0200

    Updates
    Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
 
-const App = () => {
-	return (
-		<div className='app'>
-			<div classNAme='navbar'>
-				<Navbar />
-			</div>
-			<div classNAme='main'></div>
-			<div classNAme='footer'></div>
+import {
+	Exchanges,
+	Homepage,
+	News,
+	Cryptocurrencies,
+	CryptoDetails,
+	Navbar,
+} from './components';
+import './App.css';
+
+const App = () => (
+	<div className='app'>
+		<div className='navbar'>
+			<Navbar />
 		</div>
-	);
-};
+		<div className='main'>
+			<Layout>
+				<div className='routes'>
+					<Switch>
+						<Route exact path='/'>
+							<Homepage />
+						</Route>
+						<Route exact path='/exchanges'>
+							<Exchanges />
+						</Route>
+						<Route exact path='/cryptocurrencies'>
+							<Cryptocurrencies />
+						</Route>
+						<Route exact path='/crypto/:coinId'>
+							<CryptoDetails />
+						</Route>
+						<Route exact path='/news'>
+							<News />
+						</Route>
+					</Switch>
+				</div>
+			</Layout>
+		</div>
+	</div>
+);
 
 export default App;
